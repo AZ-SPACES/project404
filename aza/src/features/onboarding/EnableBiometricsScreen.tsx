ebacb51b6714cf7869b5717fe92fb227ffa8f5ed
Caleb Dussey <119165434+semekork@users.noreply.github.com>
2026-03-20 22:14:32 +0000

    updated routes

--- aza/src/features/onboarding/EnableBiometricsScreen.tsx
+++ aza/src/features/onboarding/EnableBiometricsScreen.tsx
@@ -26,12 +26,12 @@ export default function EnableBiometricsScreen() {
   const handleSetup = () => {
     // Logic to Set up biometrics
     console.log('Set up biometrics');
-    // Proceed to PEP Status check after biometric setup
-    navigation.navigate('PEPStatus');
+    // Proceed to identity verification check after biometric setup
+    navigation.navigate('VerifyIdentity', { isPEP: false });
   };
 
   const handleNotNow = () => {
-    navigation.navigate('PEPStatus');
+    navigation.navigate('VerifyIdentity', { isPEP: false });
   };
 
   return (
