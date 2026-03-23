6d1d0656af9cd61a0af54ea3344972f2feaaa416
paapacobbold <paapacobbold1234@gmail.com>
2026-03-23 04:06:43 -0700

    feat: implement money transfer feature including send and request amount and contact screens.

---
+++ aza/src/features/transfer/index.ts
@@ -0,0 +1,4 @@
+export { default as SendContactScreen } from './send/SendContactScreen';
+export { default as SendAmountScreen } from './send/SendAmountScreen';
+export { default as RequestContactScreen } from './request/RequestContactScreen';
+export { default as RequestAmountScreen } from './request/RequestAmountScreen';
